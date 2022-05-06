8fc69e6fbcbce859446d83711a9d166c3b2831c3
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-05-06 15:34:43 +0200

    Update opus_config.h

--- src/opus_config.h
+++ src/opus_config.h
@@ -13,35 +13,35 @@
 /* Do not build the float API */
 #define DISABLE_FLOAT_API 1
 
-/* Disable bitstream fixes from RFC 8251 */
-#undef DISABLE_UPDATE_DRAFT
+// /* Disable bitstream fixes from RFC 8251 */
+// #undef DISABLE_UPDATE_DRAFT
 
 /* Assertions */
 #define ENABLE_ASSERTIONS 1
 
-/* Hardening */
-#undef ENABLE_HARDENING
+// /* Hardening */
+// #undef ENABLE_HARDENING
 
-/* Debug fixed-point implementation */
-#undef FIXED_DEBUG
+// /* Debug fixed-point implementation */
+// #undef FIXED_DEBUG
 
 /* Compile as fixed-point (for machines without a fast enough FPU) */
 #define FIXED_POINT 1
 
-/* Float approximations */
-#undef FLOAT_APPROX
+// /* Float approximations */
+// #undef FLOAT_APPROX
 
-/* Fuzzing */
-#undef FUZZING 
+// /* Fuzzing */
+// #undef FUZZING 
 
-/* Define to 1 if you have the <alloca.h> header file. */
-#undef HAVE_ALLOCA_H
+// /* Define to 1 if you have the <alloca.h> header file. */
+// #undef HAVE_ALLOCA_H
 
-/* NE10 library is installed on host. Make sure it is on target! */
-#undef HAVE_ARM_NE10
+// /* NE10 library is installed on host. Make sure it is on target! */
+// #undef HAVE_ARM_NE10
 
-/* Define to 1 if you have the <dlfcn.h> header file. */
-#undef HAVE_DLFCN_H
+// /* Define to 1 if you have the <dlfcn.h> header file. */
+// #undef HAVE_DLFCN_H
 
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
@@ -61,26 +61,26 @@
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
 
-/* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
+// /* Define to 1 if you have the <strings.h> header file. */
+// #undef HAVE_STRINGS_H
 
-/* Define to 1 if you have the <string.h> header file. */
-#undef HAVE_STRING_H
+// /* Define to 1 if you have the <string.h> header file. */
+// #undef HAVE_STRING_H
 
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#undef HAVE_SYS_STAT_H
+// /* Define to 1 if you have the <sys/stat.h> header file. */
+// #undef HAVE_SYS_STAT_H
 
-/* Define to 1 if you have the <sys/types.h> header file. */
-#undef HAVE_SYS_TYPES_H
+// /* Define to 1 if you have the <sys/types.h> header file. */
+// #undef HAVE_SYS_TYPES_H
 
-/* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
+// /* Define to 1 if you have the <unistd.h> header file. */
+// #undef HAVE_UNISTD_H
 
-/* Define to 1 if you have the `__malloc_hook' function. */
-#undef HAVE___MALLOC_HOOK
+// /* Define to 1 if you have the `__malloc_hook' function. */
+// #undef HAVE___MALLOC_HOOK
 
-/* Define to the sub-directory where libtool stores uninstalled libraries. */
-#undef LT_OBJDIR
+// /* Define to the sub-directory where libtool stores uninstalled libraries. */
+// #undef LT_OBJDIR
 
 // /* Make use of ARM asm optimization */
 // #undef OPUS_ARM_ASM
@@ -178,11 +178,11 @@
 // /* Define to 1 if you have the ANSI C header files. */
 // #undef STDC_HEADERS
 
-/* Make use of alloca */
-#undef USE_ALLOCA 
+// /* Make use of alloca */
+// #undef USE_ALLOCA 
 
-/* Use C99 variable-size arrays */
-#undef VAR_ARRAYS 
+// /* Use C99 variable-size arrays */
+// #undef VAR_ARRAYS 
 
 /* Simulated Stack using the Heap */ 
 #define NONTHREADSAFE_PSEUDOSTACK 1
@@ -190,32 +190,32 @@
 /* Simulated Stack Size using the Heap */ 
 #define GLOBAL_STACK_SIZE 60000
 
-/* Define to empty if `const' does not conform to ANSI C. */
-#undef const
+// /* Define to empty if `const' does not conform to ANSI C. */
+// #undef const
 
-/* Define to `__inline__' or `__inline' if that's what the C compiler
-   calls it, or to nothing if 'inline' is not supported under any name.  */
-#ifndef __cplusplus
-#undef inline
-#endif
+// /* Define to `__inline__' or `__inline' if that's what the C compiler
+//    calls it, or to nothing if 'inline' is not supported under any name.  */
+// #ifndef __cplusplus
+// #undef inline
+// #endif
 
-/* Define to the equivalent of the C99 'restrict' keyword, or to
-   nothing if this is not supported.  Do not define if restrict is
-   supported directly.  */
-#undef restrict
-/* Work around a bug in Sun C++: it does not support _Restrict or
-   __restrict__, even though the corresponding Sun C compiler ends up with
-   "#define restrict _Restrict" or "#define restrict __restrict__" in the
-   previous line.  Perhaps some future version of Sun C++ will work with
-   restrict; if so, hopefully it defines __RESTRICT like Sun C does.  */
-#if defined __SUNPRO_CC && !defined __RESTRICT
-# define _Restrict
-# define __restrict__
-#endif
+// /* Define to the equivalent of the C99 'restrict' keyword, or to
+//    nothing if this is not supported.  Do not define if restrict is
+//    supported directly.  */
+// #undef restrict
+// /* Work around a bug in Sun C++: it does not support _Restrict or
+//    __restrict__, even though the corresponding Sun C compiler ends up with
+//    "#define restrict _Restrict" or "#define restrict __restrict__" in the
+//    previous line.  Perhaps some future version of Sun C++ will work with
+//    restrict; if so, hopefully it defines __RESTRICT like Sun C does.  */
+// #if defined __SUNPRO_CC && !defined __RESTRICT
+// # define _Restrict
+// # define __restrict__
+// #endif
 
 
 /* Do not build reading support */
-#define OGGZ_CONFIG_READ 0
+#define OGGZ_CONFIG_READ 1
 
 /* Do not build writing support */
-#define OGGZ_CONFIG_WRITE 0
+#define OGGZ_CONFIG_WRITE 1
